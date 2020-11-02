d8a4a3598ae5b2731667198dbe93dbf1c32b4c3f
huangan <huanganojyaa.@163.com>
2020-11-02 10:31:26 +0800

    这是第三个版本  san

--- Hellogit/src/cn/itzeta/hellogit.java
+++ Hellogit/src/cn/itzeta/hellogit.java
@@ -4,5 +4,6 @@ public class hellogit {
     public static void main(String[] args) {
         System.out.println("this is first version！");
         System.out.println("xiugai di er ban");
+        System.out.println("san");
     }
 }
